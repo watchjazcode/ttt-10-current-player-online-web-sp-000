e967c5c42cb3a794ce6ca2d116294ba314821883
Jazmin M <jazminmerckel@gmail.com>
2019-09-16 11:00:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,10 @@
 #writing two methods:
 #one that will tell us how many turns have been played
 #another that will return an X or an O, based on that information
+
+def turn_count(board)
+
+end
+
+
+def current_player
