170cb71799ec9b22114b394fbebec2eea3de5306
Jazmin M <jazminmerckel@gmail.com>
2019-09-16 11:28:24 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,14 +2,13 @@
 #one that will tell us how many turns have been played
 #another that will return an X or an O, based on that information
 
-def turn_count(board)
-  counter = 0
-  # this is what board will look like, but will not be run
+def turn_count(board) #method that determines how many turns have been played
+  counter = 0 #starts at 0 and is how many turns have been played so far
+  #this is what board will look like, but will not be run
   #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  board.each do |space|
-  counter += 
-end
-
+  board.each do |space| #checking each space individually
+  if position_taken?(board)
+    counter += 1 #everytime we look at ea space, if taken we played one turn
   end
 end
 
